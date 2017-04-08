c623ff1c94e670badcf3e0a763ac935deb23f6f1
shaiArnon <sharnon1@gmail.com>
2017-04-08 18:32:32 +0300

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,62 +1,62 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
-            var now = new Date().toString();
-            var log = `${now}: ${req.method} ${req.url}`
-            console.log(log);
-            fs.appendFile('server.log', log + '\n', (err) => {
-                if (err) {
-                    console.log('Unable to append to server.log.')
-                }
-            });
-            next();
-            // });
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.')
+        }
+    });
+    next();
+});
 
-            // app.use((req, res, next) => {
-            // res.render('maintenance.hbs', {
-            //         pageTitle: 'Under maintenance',
-            //         maintenanceMessage: 'This website is under maintenance'
-            //     });
-            // });
+// app.use((req, res, next) => {
+// res.render('maintenance.hbs', {
+//         pageTitle: 'Under maintenance',
+//         maintenanceMessage: 'This website is under maintenance'
+//     });
+// });
 
-            app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-            hbs.registerHelper('getCurrentYear', () => {
-                return new Date().getFullYear();
-            });
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
 
-            hbs.registerHelper('screamIt', (text) => {
-                return text.toUpperCase();
-            });
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 
-            app.get('/', (req, res) => {
-                // res.send('<h1>Hello Express!</h1>');
-                res.render('home.hbs', {
-                    pageTitle: 'Home Page',
-                    welcomeMessage: 'Welcome to my website!'
-                });
-            });
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website!'
+    });
+});
 
-            app.get('/about', (req, res) => {
-                res.render('about.hbs', {
-                    pageTitle: 'About Page',
-                });
-            });
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
 
 
-            app.get('/bad', (req, res) => {
-                res.send({
-                    errorMessage: 'bad request'
-                });
-            });
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'bad request'
+    });
+});
 
-            app.listen(3000, () => {
-                console.log('Server is up on port 3000');
-            });
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
